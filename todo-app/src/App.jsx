d11f93195e6f18e48f8d195a2f3f8919515f4585
edBatiancila <batiancilaedmyr@gmail.com>
2024-12-03 12:05:59 +0800

    added updates in my  code.

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import Another from './Another';
 
 function App() {
   const [count, setCount] = useState(0);
 
   const increment = () => {
-      setCount(count + 1);
-  }
+    setCount(prevCount => prevCount + 1);
+  };
 
   const decrement = () => {
-    setCount(count - 1);
-  }
+    setCount(prevCount => prevCount - 1);
+  };
 
   const someStyle = {
     background: 'blue',
@@ -20,35 +21,33 @@ function App() {
     fontWeight: 'bold',
     padding: '10px',
     borderRadius: '10px',
-    cursor: 'pointer'
+    cursor: 'pointer',
   };
 
   const incDec = {
-    display: "flex",
-    flexDirection: "column"
-  }
+    display: 'flex',
+    flexDirection: 'column',
+  };
 
   const buttonSpacing = {
-    padding: "10px"
-  }
+    padding: '10px',
+  };
   return (
     <div className="App">
       <header className="App-header">
+        <Another name="Edmyr" />
         <div style={incDec}>
           <span>{count}</span>
           <div style={buttonSpacing}>
             <button onClick={increment}>+</button>
             <button onClick={decrement}>-</button>
           </div>
-          
         </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        { true &&
-          <p style={someStyle}>This is my style</p>
-        }
+        {true && <p style={someStyle}>This is my style</p>}
         <a
           className="App-link"
           href="https://reactjs.org"

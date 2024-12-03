dbda53021d4326144e1fbedb1858652d215df7b9
edBatiancila <batiancilaedmyr@gmail.com>
2024-12-03 17:06:11 +0800

    updated the code in this repo.

---
+++ Scrimba/markupproject/src/index.js
@@ -0,0 +1,29 @@
+
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+
+root.render(
+    <FunFact />
+);
+
+
+function FunFact() {
+    return(
+        <main>
+            <img src="/logo512.png" alt="logo" width="40px" />
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
+

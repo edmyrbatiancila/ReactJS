398fe31f3529d0ef0e0de70e133e3bdb1d47a379
edBatiancila <batiancilaedmyr@gmail.com>
2024-12-03 11:09:18 +0800

    updated some codes.

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,65 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+      setCount(count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
+  const someStyle = {
+    background: 'blue',
+    color: 'white',
+    fontSize: '28px',
+    fontWeight: 'bold',
+    padding: '10px',
+    borderRadius: '10px',
+    cursor: 'pointer'
+  };
+
+  const incDec = {
+    display: "flex",
+    flexDirection: "column"
+  }
+
+  const buttonSpacing = {
+    padding: "10px"
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={incDec}>
+          <span>{count}</span>
+          <div style={buttonSpacing}>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+          </div>
+          
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        { true &&
+          <p style={someStyle}>This is my style</p>
+        }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
